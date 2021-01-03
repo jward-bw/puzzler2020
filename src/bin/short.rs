2b6c5660a4d0c98a26092b5584b03cdb8ac07637
Jacob Ward <jward@brandwatch.com>
2021-01-03 14:57:49 +0000

    short: Faster and shorter.

--- src/bin/short.rs
+++ src/bin/short.rs
@@ -1,5 +1,3 @@
-use std::io::*;static G:&str="rownsholrelegontoecoshgcrbyntybltboyswgwhc";fn main(){BufReader::new(
-std::fs::File::open("/usr/share/dict/words").unwrap()).lines().for_each(|w|q(w.unwrap()));}
-fn q(w:String){for p in w.chars().collect::<Vec<char>>().windows(2){if G.contains(&c(p)){continue};
-let mut p=p.to_owned();p.reverse();if !G.contains(&c(p.as_slice())){return};}println!("{}",w);}
-fn c(s:&[char])->String{s.iter().collect()}
+use std::io::*;static G:&str="chsybrcrbyshcerlbtynswhgcghwsnytblrecoeltnwgegwntleorolobotoyonosowohogoc";
+fn main(){BufReader::new(std::fs::File::open("/usr/share/dict/words").unwrap()).lines().for_each(|w|q(w.unwrap()));}
+fn q(w:String){for p in w.chars().collect::<Vec<char>>().windows(2){if !G.contains(&p.iter().collect::<String>()){return}}println!("{}",w);}
